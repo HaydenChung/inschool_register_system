6ede9cc7a5fe6d31d0c19e050643864bbe0722f6
Hayden <haydenchung@ymail.com>
2018-12-27 10:11:06 +0800

    26/12/2018 work

--- inschool_register/src/js/component/container/RegisterPage.js
+++ inschool_register/src/js/component/container/RegisterPage.js
@@ -14,10 +14,28 @@ class RegisterPage extends React.Component {
             studentData: {},
             active: true,
             isVerify: false,
+            freePassLock: false,
         }
+
+        this.freePassTimer = null;
 
         this.hiddenInputRef = React.createRef();
         this.inputOnKeyPressHandler = this.inputOnKeyPressHandler.bind(this);
+        this.toggleFreePassPeriod = this.toggleFreePassPeriod.bind(this);
+        this.onRegisterFailHandler = this.onRegisterFailHandler.bind(this);
+    }
+
+    toggleFreePassPeriod({expire= 30, state}) {
+        clearTimeout(this.freePassTimer);
+
+        if(state != true) {
+            this.setState({freePassLock: false});
+
+            return;
+        }
+
+        this.setState({freePassLock: true});
+        this.freePassTimer = setTimeout(()=> this.setState({freePassLock: true}), expire* 1000);
     }
 
     inputOnKeyPressHandler(ev) {
@@ -28,6 +46,13 @@ class RegisterPage extends React.Component {
             const cardId = ev.target.value;
             ev.target.value = '';
             let isVerify = false;
+
+
+            if(this.state.freePassLock === true) {
+
+            } else {
+                
+            }
 
             ajaxCall({
                 action: 'fetch_night_in_school_student_registered',
@@ -60,9 +85,9 @@ class RegisterPage extends React.Component {
                             }).then(response=> {
                                 let student = {};
                                 Object.keys(response).some((key)=> {
-                                    student = response[key];
+                                    student = Object.assign(response[key], isVerify);
                                 });
-                                this.setState({studentData: student, active: true, isVerify});
+                                this.setState({studentData: student, active: true});
                             });
                         }
                     });
@@ -79,14 +104,70 @@ class RegisterPage extends React.Component {
                     }).then(response => {
                         let student = {};
                         Object.keys(response).some((key)=> {
-                            student = response[key];
+                            student = Object.assign(response[key], isVerify);
                         });
-                        this.setState({studentData: student, active: true, isVerify});
+                        this.setState({studentData: student, active: true});
                     });
 
                 }
             });
         }
+    }
+
+    checkWhiteList(cardId){
+
+        return ajaxCall({
+            action: 'fetch_night_in_school_student_registered',
+            postData: {
+                'card_number': cardId,
+            }
+        })
+        .then(response => {
+            if(!response[0].uid) {
+                let error = new Error("No result return from register list.");
+                error.code = "NOT_IN_WHITELIST";
+                error.userCardId = cardId;
+                throw error;
+            }
+        })
+    }
+
+    fetchUser(cardId, isVerify) {
+
+        return ajaxCall({
+            action: 'fetch_user_static',
+            postData: {
+                'conditions': JSON.stringify({
+                    field: 'card_id',
+                    params: cardId,
+                    operator: '='
+                })
+            }
+        }).then(response => {
+            let student = {};
+            Object.keys(response).some((key)=> {
+                student = Object.assign(response[key], isVerify);
+            });
+            this.setState({studentData: student, active: true});
+        })
+        .catch(this.onRegisterFailHandler);
+
+    }
+
+
+
+    onRegisterFailHandler(error) {
+
+        switch error.code {
+            case 'NOT_IN_WHITELIST':
+                this.fetchUser(error.userCardId,false);
+                return;
+            break;
+        }
+
+        reminder('Register Failed, please check your connection or contact support staff.');
+
+        this.setState({studentData: {}, active: true});
     }
 
     render() {
@@ -98,7 +179,7 @@ class RegisterPage extends React.Component {
             <Template active={this.state.active}>
                 <div style={{display: 'flex', alignContent: 'center', justifyContent: 'center'}}>
                     <HiddenInput disabled={!this.state.active} onKeyPress={this.inputOnKeyPressHandler} ref={this.hiddenInputRef}/>
-                    <LargeUserCard chi_name={student.chi_name} eng_name={student.eng_name} class={student.class} class_number={student.class_number} image={student.uri} student_no={student.name} />
+                    <LargeUserCard freePassLock={this.state.freePassLock} chi_name={student.chi_name} eng_name={student.eng_name} class={student.class} class_number={student.class_number} image={student.uri} student_no={student.name} />
                 </div>
             </Template>
         )
